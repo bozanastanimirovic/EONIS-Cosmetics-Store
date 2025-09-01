fdec9b123ae4d407b522053abee6790c2b3570f7
bozana <bozana@BOZANA-PC>
2025-09-01 18:45:59 +0200

    Routing and error handling

--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon'
 import { MatButton } from '@angular/material/button'
 import { MatBadge } from '@angular/material/badge'
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { MatProgressBar} from "@angular/material/progress-bar";
+import { MatProgressSpinner} from "@angular/material/progress-spinner";
+import { BusyService } from '../../core/services/busy.service';
 
 @Component({
   selector: 'app-header',
   imports: [
     MatIcon,
     MatButton,
-    MatBadge
-  ],
+    MatBadge,
+    RouterLink,
+    RouterLinkActive,
+    MatProgressBar
+],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  busyService = inject(BusyService);
 }
